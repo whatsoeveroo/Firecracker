7c6b59e98d788af457b4802b9565e961f3818237
whatsoeveroo <soundwave0524@gmail.com>
2026-05-02 17:23:59 +0900

    Ignore local worktrees in ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,12 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores([
+    'dist/**',
+    'node_modules/**',
+    '.claude/**',
+    'Firecracker/**',
+  ]),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
